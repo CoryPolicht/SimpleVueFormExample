4ece72cac2f14f079190df0f1f692fadf4de24ac
Cory <cory.policht@gmail.com>
2019-12-22 17:25:15 -0600

    quick example form

---
+++ vue-form-example/src/components/HelloWorld.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="hello">
+    <h3>{{msg}}</h3>
+    <tr>
+      <td>
+        <label>bound text input</label>
+      </td>
+      <td>
+        <input type="text" v-model="exampleBinding.data" />
+      </td>
+      <td>
+        <button v-on:click="submitted">Submit</button>
+      </td>
+    </tr>
+    <br />
+    <br />
+    <tr>
+      <td>
+        <label>Submit data: {{ dataSubmitObject }}</label>
+      </td>
+    </tr>
+  </div>
+</template>
+
+<script>
+// import axios from "axios";
+
+export default {
+  name: "HelloWorld",
+  data () {
+    return {
+      exampleBinding: {
+        data: ""
+      },
+      dataSubmitObject: {
+        data: ""
+
+      }
+    }
+  },
+  methods : {
+    submitted() {
+      let json = JSON.stringify(this.exampleBinding);
+      this.dataSubmitObject = json;
+      // axios.post("<URL>", this.dataSubmit);
+    }
+  },
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+tr {
+  list-style-type: none;
+  padding: 0;
+}
+td {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
